cd172c85208ec98d3b8f8b4294ab802aa126fc76
kiruba <test@gamil.com>
2025-09-05 11:49:04 +0530

    three

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { SiAmazonaws, SiMicrosoftazure, SiJenkins, SiPrometheus } from "react-icons/si";
 import { SiDocker, SiKubernetes, SiGit, SiTerraform, SiAnsible, SiGrafana, SiHelm, SiGithub, SiNginx } from "react-icons/si";
-import { CgCPlusPlus } from "react-icons/cg";
+// import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
